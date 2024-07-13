839ca75aab4a086ead1395643d18d844e1e071c4
santaugusto <sanntaugusto@gmail.com>
2024-07-13 16:39:59 -0300

    :wastebasket: remove: iniciar do zero

--- src/funcionarios/dto/update-funcionario.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFuncionarioDto } from './create-funcionario.dto';
-
-export class UpdateFuncionarioDto extends PartialType(CreateFuncionarioDto) {}
--- src/funcionarios/funcionarios.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FuncionariosService } from './funcionarios.service';
-import { FuncionariosController } from './funcionarios.controller';
-
-@Module({
-  controllers: [FuncionariosController],
-  providers: [FuncionariosService],
-})
-export class FuncionariosModule {}
